bf82b6bdc27abb7a456cf093d05d7c5bd013e06e
Amara Liz <aammaaraalliizzx@gmail.com>
2025-10-04 14:30:42 -0400

    Segurança

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,12 @@
 
 import Header from "@/components/header";
 import Particles from "@/components/Particles";
+import { Button } from "@heroui/button";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div style={{ width: "100%", height: "600px", position: "relative" }}>
-        <Particles
-          particleColors={["#ffffff", "#ffffff"]}
-          particleCount={200}
-          particleSpread={10}
-          speed={0.1}
-          particleBaseSize={100}
-          moveParticlesOnHover={true}
-          alphaParticles={false}
-          disableRotation={false}
-        />
-      </div>
     </>
   );
 }
