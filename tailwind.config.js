1deb9f92f4fd1109659233a6360b72d57de0f6b9
Amara Liz <aammaaraalliizzx@gmail.com>
2025-10-04 14:13:22 -0400

    Commit segurança

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+// tailwind.config.js
+const {heroui} = require("@heroui/theme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        shine: {
+          '0%': { 'background-position': '100%' },
+          '100%': { 'background-position': '-100%' },
+        },
+      },
+      animation: {
+        shine: 'shine 5s linear infinite',
+      },
+      fontFamily: {
+        sans: ['var(--font-poppins)', 'sans-serif'],
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [heroui()],
+};
+
+
+// tailwind.config.js
+// module.exports = {
+//   theme: {
+//     extend: {
+//       keyframes: {
+//         shine: {
+//           '0%': { 'background-position': '100%' },
+//           '100%': { 'background-position': '-100%' },
+//         },
+//       },
+//       animation: {
+//         shine: 'shine 5s linear infinite',
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
