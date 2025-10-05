78cdbc0bca05732b9ca4d4e43954a596139889b6
Amara Liz <aammaaraalliizzx@gmail.com>
2025-10-04 22:06:30 -0400

    Segurança

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,16 @@
 import { Focus, Lightbulb, Users } from "lucide-react";
 
 const Footer = () => {
+  function handleAnimationComplete(): void {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <footer className="bg-gray-900/50 py-20 px-4">
       <div className="max-w-7xl mx-auto">
         <div className="text-center mb-16">
           <h2 className="text-white text-4xl md:text-5xl font-bold mb-4">
-            Find your perfect fit & grow with us
+            Lorem ipsum dolor sit amet
           </h2>
           <p className="text-gray-300 text-lg md:text-xl max-w-3xl mx-auto">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -18,11 +22,10 @@ const Footer = () => {
               <Focus className="w-8 h-8 text-white" />
             </div>
             <h3 className="text-white text-2xl font-semibold mb-2">
-              Focus & Productivity
+              Lorem ipsum 
             </h3>
             <p className="text-gray-400">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             </p>
           </div>
           <div className="flex flex-col items-center">
@@ -30,11 +33,10 @@ const Footer = () => {
               <Lightbulb className="w-8 h-8 text-white" />
             </div>
             <h3 className="text-white text-2xl font-semibold mb-2">
-              Spark Innovation
+              Lorem ipsum 
             </h3>
             <p className="text-gray-400">
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             </p>
           </div>
           <div className="flex flex-col items-center">
@@ -42,11 +44,10 @@ const Footer = () => {
               <Users className="w-8 h-8 text-white" />
             </div>
             <h3 className="text-white text-2xl font-semibold mb-2">
-              Supportive Community
+              Lorem ipsum 
             </h3>
             <p className="text-gray-400">
-              Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             </p>
           </div>
         </div>
